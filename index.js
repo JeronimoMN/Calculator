f0873b989178057c5ea9cec30090e9608d290dfb
jeroMuriNarva <jeronimomurillonarvaez@gmail.com>
2025-01-20 21:58:55 -0500

    Changes in the history logic

--- index.js
+++ index.js
@@ -4,9 +4,17 @@ class Result {
         this.operator = ''
         this.historial = []
     }
+
+    logOperation(operation, result, event) {
+        this.historial.push({ operation, result, event })
+    }
+
+    getHistory() {
+        return this.historial
+    }
 }
 
-function initCalculator(){
+function initCalculator() {
     const rs = new Result()
 
     const resultElement = document.getElementById('result')
@@ -14,22 +22,10 @@ function initCalculator(){
     const operators = document.querySelectorAll(".operator")
     const cleanDisplay = document.querySelector(".clean")
     const equal = document.querySelector(".equal")
-    const history = document.querySelector('.history')
+    const historyButtom = document.querySelector('.history')
 
-    // const dark = document.getElementById('chgcolor')
-
-    // dark.addEventListener('click', e =>{
-    //     document.body.style.backgroundColor = 'black'
-    // })
-
-    history.addEventListener('click', e => {
-        console.log(rs.historial)
-
-    })
-
-    
     numbers.forEach((key) => {
-        key.addEventListener("click", (event) =>{
+        key.addEventListener("click", (event) => {
             rs.value += event.target.value
             resultElement.textContent = rs.value
         })
@@ -46,22 +42,23 @@ function initCalculator(){
         clean(rs)
     })
 
-    equal.addEventListener("click", e =>{
+    // Perform Calculation and log history
+    equal.addEventListener("click", e => {
 
         /*
-        Save: Operation, Result and Event
-        */
+       Save: Operation, Result and Event
+       */
 
-        const operation = {}
-
-
-        operation['operation'] = rs.value
+        const operation = rs.value;
         operate(rs)
-        operation['result'] = rs.value
-        operation['event'] = e.target.className
+        rs.logOperation(operation, rs.value, e.target.className)
+        resultElement.textContent = rs.value
+    })
 
-        rs.historial.push(operation)
-        resultElement.textContent= rs.value
+
+    // Display history in the console
+    historyButtom.addEventListener('click', (e) => {
+        console.log('History:', JSON.stringify(rs.getHistory(), null, 2))
     })
 
 }
@@ -70,30 +67,29 @@ function initCalculator(){
 /*
     Get Result
     Convert an string into substrings using an separator
+    Problem inside of the function: The code repeat itself in each conditional- How can i improve it?
 */
 
-function operate(rs){
-
-
-    if(rs.value.includes("+")){
+function operate(rs) {
+    if (rs.value.includes("+")) {
         let exprArr = rs.value.split('+')
-        let a = parseInt( exprArr[0])
+        let a = parseInt(exprArr[0])
         let b = parseInt(exprArr[1])
         sumNumbers(a, b, rs)
-    }else if(rs.value.includes("-")){
+    } else if (rs.value.includes("-")) {
         let exprArr = rs.value.split('-')
-        let a = parseInt( exprArr[0])
+        let a = parseInt(exprArr[0])
         let b = parseInt(exprArr[1])
         console.log(a, b)
         subtractNumbers(a, b, rs)
-    }else if(rs.value.includes("*")){
+    } else if (rs.value.includes("*")) {
         let exprArr = rs.value.split('*')
-        let a = parseInt( exprArr[0])
+        let a = parseInt(exprArr[0])
         let b = parseInt(exprArr[1])
         multiplyNumbers(a, b, rs)
-    }else if(rs.value.includes("/")){
+    } else if (rs.value.includes("/")) {
         let exprArr = rs.value.split('/')
-        let a = parseInt( exprArr[0])
+        let a = parseInt(exprArr[0])
         let b = parseInt(exprArr[1])
         divideNumbers(a, b, rs)
     }
@@ -103,40 +99,34 @@ function operate(rs){
     Clean input
 */
 
-function clean(rs){
+function clean(rs) {
     document.getElementById('result').textContent = ''
     rs.value = ''
 }
 
-
-
 /*
-
-BASIC FUNCTIONS
-
+    Basic Functions
 */
-function sumNumbers(number1, number2, rs){
+function sumNumbers(number1, number2, rs) {
     clean(rs)
     rs.value += number1 + number2;
 
 }
 
-function subtractNumbers(number1, number2, rs){
+function subtractNumbers(number1, number2, rs) {
     clean(rs)
     rs.value += number1 - number2
 }
 
-function multiplyNumbers(number1, number2, rs ){
+function multiplyNumbers(number1, number2, rs) {
     clean(rs)
     rs.value += number1 * number2;
 }
 
-function divideNumbers(number1, number2, rs ){
+function divideNumbers(number1, number2, rs) {
     clean(rs)
     rs.value += number1 / number2;
 }
 
 
 initCalculator()
-
-
